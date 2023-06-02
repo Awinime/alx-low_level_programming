aedf70dea594aae5910bcb1d2c0146d39ee29dfb
Awinime <awinime88@gmail.com>
2023-06-02 13:36:46 -0700

    list len updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include "lists.h"
 
 /**
-list_len - numb of elements in a linked list is returned
- * @h: the list_t list pointer
- *
- * Return:returns numb of elements in h
- */
+*list_len - numb of elements in a linked list is returned
+* @h: the list_t list pointer
+*
+* Return:returns numb of elements in h
+*/
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+while (h)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
